483473eefe248f978233485802a9485f47345944
Trie_Chen <trie55432@gmail.com>
2025-02-18 20:46:29 +0800

    feat: change to TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import OrderBook from "./components/OrderBook";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-[#131B29] flex items-center justify-center">
+      <OrderBook />
+    </div>
+  );
+};
+
+export default App;
